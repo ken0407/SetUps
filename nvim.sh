2f47cdfe4106b3a186aab9384f5f6b16b7f14dfe
keeeeen <41322996+ken0407@users.noreply.github.com>
2020-08-03 21:59:26 +0900

    Update nvim.sh

--- nvim.sh
+++ nvim.sh
@@ -6,6 +6,8 @@ npm install -g neovim
 pip3 install pynvim
 echo "XDG_BASE_HOME='~/.config'" >> ~/.bash_profile
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+git clone https://github.com/ken0407/nvim.git ~/.config/nvim
 sh ./installer.sh ~/.config/nvim/dein
 echo "alias vim='nvim'" >> ~/.bash_profile
+echo "alias vi='nvim'" >> ~/.bash_profile
 echo "alias vimcf='vim ~/.config/nvim'" >> ~/.bash_profile
